4ac51f7308f85c898619baa6e12bdcccc6521b97
Franco <francoelsasso@gmail.com>
2020-09-25 00:18:37 -0300

    Commit de primeras operaciones

---
+++ Archivo_1.java
@@ -0,0 +1,9 @@
+public class Archivo_1 {
+	
+	public static void main(String args []) {
+		String contenido;
+		contenido = "Archivo nro 1";
+		System.out.print(contenido);
+	}
+	
+}
---
+++ Archivo_2.java
@@ -0,0 +1,9 @@
+public class Archivo_2 {
+
+	public static void main(String args []) {
+		Object objeto = new Object();
+		objeto = (String) objeto;
+		objeto = "Archivo nro 2";
+	}
+	
+}
