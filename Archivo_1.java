a37ada4adca59b894b528bfd72ad4010969b07f1
Franco <francoelsasso@gmail.com>
2020-09-25 00:49:30 -0300

    Commit conflictivo

--- Archivo_1.java
+++ Archivo_1.java
@@ -2,7 +2,7 @@ public class Archivo_1 {
 	
 	public static void main(String args []) {
 		String contenido;
-		contenido = "Archivo nro 1";
+		contenido = "Archivo número uno";
 		System.out.print(contenido);
 	}
 	
